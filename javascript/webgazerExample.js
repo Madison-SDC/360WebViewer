fbc9a4aaf8050518f27fed41800d9884b806035e
jsimonson <jsimonson2013@gmail.com>
2016-09-25 13:54:28 -0500

    Logging x and y coordinates of the gaze point. Will be able to use these two parameters for 360 content detection and logic.

--- javascript/webgazerExample.js
+++ javascript/webgazerExample.js
@@ -2,7 +2,7 @@ window.onload = function() {
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
-         //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+         if(data != null) console.log(data.x,data.y); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .begin()
