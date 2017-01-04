a79a348ee3aa35fce9b77bfe1b7649750f3e936c
jsimonson <jsimonson2013@gmail.com>
2017-01-04 14:53:16 -0600

    Commented code. Possibly final commit in this Repo.

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,4 @@
 require('aframe');
 require('../index.js');
-require('../styles/sample.css');
 require('../node_modules/webgazer/build/webgazer.js');
 require('../node_modules/aframe/build/aframe.js');
